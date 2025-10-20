da61f04ee08b62b7f3a680f1d5d21d2fea281179
The Daniel <dmnyc@users.noreply.github.com>
2025-10-20 15:32:15 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
